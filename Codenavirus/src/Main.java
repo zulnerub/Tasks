c0f0cf009feca2027700b80ac5bb791ca3603d1c
Simeon <drsimeon_87@abv.bg>
2020-07-12 16:18:34 +0300

    07122020
    07122020

--- Codenavirus/src/Main.java
+++ Codenavirus/src/Main.java
@@ -1,54 +1,51 @@
 import java.util.*;
 
 public class Main {
-
-    public static char[][] world;
-    public static int[][] status;
-    public static Deque<int[]> allInfectedCoordinates = new ArrayDeque<>();
-    public static int[] firstInfected = new int[2];
-    public static int countDays = 0;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<char[]> rows = new ArrayList<>();
-        String current;
 
+        List<String> rows = new ArrayList<>();
+
+        String current;
         while (!(current = sc.nextLine()).isEmpty()) {
-            rows.add(current.toCharArray());
+            rows.add(current);
         }
 
-        char[] infectedPosition = rows.get(rows.size() - 1);
+        int[] firstInfected = Arrays.stream(rows.get(rows.size() - 1).split("\\s+|,| ,"))
+                .mapToInt(Integer::parseInt).toArray();
         rows.remove(rows.size() - 1);
 
-        for (int i = 0; i < firstInfected.length; i++) {
-            firstInfected[i] = Integer.parseInt(String.valueOf(infectedPosition[i]));
-        }
-
-        world = new char[rows.size()][rows.get(0).length];
-        status = new int[rows.size()][rows.get(0).length];
-
+        char[][] world = new char[rows.size()][];
         for (int i = 0; i < rows.size(); i++) {
-            for (int j = 0; j < rows.get(i).length; j++) {
-                world[i][j] = rows.get(i)[j] == '#' ? 'H' : '.';
-                status[i][j] = world[i][j] == 'H' ? 0 : -1;
-            }
+            world[i] = rows.get(i).toCharArray();
         }
 
-        printMatrix(world);
         System.out.println(Arrays.toString(codenavirus(world, firstInfected)));
 
     }
 
     public static int[] codenavirus(char[][] world, int[] firstInfected) {
-        if (isValidPosition(firstInfected[0], firstInfected[1])){
+        Deque<int[]> allInfectedCoordinates = new ArrayDeque<>();
+        int[][] status = new int[world.length][];
+        int countDays = 0;
+
+        for (int i = 0; i < world.length; i++) {
+            status[i] = new int[world[i].length];
+            for (int j = 0; j < world[i].length; j++) {
+                world[i][j] = world[i][j] == '#' ? 'H' : '.';
+                status[i][j] = world[i][j] == 'H' ? 0 : -1;
+            }
+        }
+
+        if (isValidPosition(world, firstInfected[0], firstInfected[1])){
             countDays++;
+
             world[firstInfected[0]][firstInfected[1]] = 'I';
             status[firstInfected[0]][firstInfected[1]]++;
+
             allInfectedCoordinates.add(firstInfected);
 
-            printMatrix(world);
-
-            dayCounter();
+            countDays = dayCounter(world, status, countDays, allInfectedCoordinates);
         }
 
         int infectedAtEnd = 0;
@@ -70,12 +67,11 @@ public class Main {
         return new int[]{countDays, infectedAtEnd, recoveredAtEnd, uninfectedAtEnd};
     }
 
-    private static void dayCounter() {
-        countDays++;
+    private static int dayCounter(char[][] world, int[][] status, int countDays, Deque<int[]> allInfectedCoordinates) {
+        int newDay = countDays + 1;
         Deque<int[]> toTraverse = new ArrayDeque<>();
 
         allInfectedCoordinates.removeIf(el -> world[el[0]][el[1]] == 'R');
-
         allInfectedCoordinates.forEach(toTraverse::offer);
         allInfectedCoordinates.clear();
 
@@ -89,23 +85,23 @@ public class Main {
                 world[coordinates[0]][coordinates[1]] = 'R';
                 check--;
             } else {
-                if (isValidPosition(coordinates[0], coordinates[1] + 1)
-                                || isValidPosition(coordinates[0] - 1, coordinates[1])
-                                || isValidPosition(coordinates[0], coordinates[1] - 1)
-                                || isValidPosition(coordinates[0] + 1, coordinates[1])) {
-                    if (toInfect(coordinates[0], coordinates[1] + 1)) {
+                if (isValidPosition(world, coordinates[0], coordinates[1] + 1)
+                        || isValidPosition(world, coordinates[0] - 1, coordinates[1])
+                        || isValidPosition(world, coordinates[0], coordinates[1] - 1)
+                        || isValidPosition(world, coordinates[0] + 1, coordinates[1])) {
+                    if (toInfect(world, coordinates[0], coordinates[1] + 1)) {
                         allInfectedCoordinates.offer(new int[] {coordinates[0], coordinates[1]});
                         allInfectedCoordinates.offer(new int[]{coordinates[0], coordinates[1] + 1});
                         status[coordinates[0]][coordinates[1] + 1]++;
-                    } else if (toInfect(coordinates[0] - 1, coordinates[1])) {
+                    } else if (toInfect(world, coordinates[0] - 1, coordinates[1])) {
                         allInfectedCoordinates.offer(new int[]{coordinates[0] - 1, coordinates[1]});
                         allInfectedCoordinates.offer(new int[] {coordinates[0], coordinates[1]});
                         status[coordinates[0] - 1][coordinates[1]]++;
-                    } else if (toInfect(coordinates[0], coordinates[1] - 1)) {
+                    } else if (toInfect(world, coordinates[0], coordinates[1] - 1)) {
                         allInfectedCoordinates.offer(new int[]{coordinates[0], coordinates[1] - 1});
                         allInfectedCoordinates.offer(new int[] {coordinates[0], coordinates[1]});
                         status[coordinates[0]][coordinates[1] - 1]++;
-                    } else if (toInfect(coordinates[0] + 1, coordinates[1])) {
+                    } else if (toInfect(world, coordinates[0] + 1, coordinates[1])) {
                         allInfectedCoordinates.offer(new int[]{coordinates[0] + 1, coordinates[1]});
                         allInfectedCoordinates.offer(new int[] {coordinates[0], coordinates[1]});
                         status[coordinates[0] + 1][coordinates[1]]++;
@@ -118,18 +114,16 @@ public class Main {
             }
         }
 
-        printMatrix(world);
-
         if (check == 0){
-            return;
+            return newDay;
+        }else {
+            return dayCounter(world, status, newDay, allInfectedCoordinates);
         }
 
-
-        dayCounter();
     }
 
-    private static boolean toInfect(int row, int col) {
-        if (isValidPosition(row, col)) {
+    private static boolean toInfect(char[][] world, int row, int col) {
+        if (isValidPosition(world, row, col)) {
             world[row][col] = 'I';
             return true;
         }
@@ -137,16 +131,16 @@ public class Main {
     }
 
     private static void printMatrix(char[][] world) {
-        for (char[] chars : world) {
-            for (char aChar : chars) {
-                System.out.print(aChar + " ");
+        for (char[] row : world) {
+            for (char el : row) {
+                System.out.print(el + " ");
             }
             System.out.println();
         }
         System.out.println();
     }
 
-    private static boolean isValidPosition(int toInfectRow, int toInfectCol) {
+    private static boolean isValidPosition(char[][] world, int toInfectRow, int toInfectCol) {
         return toInfectRow >= 0
                 && toInfectRow < world.length
                 && toInfectCol >= 0
@@ -156,3 +150,7 @@ public class Main {
 
 
 }
+
+/*
+Passed tests: 0 of 5 Failed tests: #1 Input: world: [["#","#","#"], ["#","#","#"], ["#","#","#"]] firstInfected: [1, 1] Output: [1, 3, 6, 0] Expected Output: [7, 3, 6, 0] #2 Input: world: [["#","#","."], [".","#","#"], ["#",".","#"]] firstInfected: [1, 1] Output: [1, 3, 2, 1] Expected Output: [5, 3, 2, 1] #3 Input: world: [["#",".","."], [".","#","."], ["#",".","#"]] firstInfected: [1, 1] Output: [1, 1, 0, 3] Expected Output: [2, 1, 0, 3] #4 Input: world: [["#","#","#","#","#","#","#"], ["#","#","#","#","#","#","#"], ["#","#","#","#","#","#","#"], ["#","#","#","#","#","#","#"], ["#","#","#","#","#","#","#"], ["#","#","#","#","#","#","#"], ["#","#","#","#","#","#","#"], ["#","#","#","#","#","#","#"], ["#","#","#","#","#","#","#"]] firstInfected: [1, 1] Output: [1, 13, 50, 0] Expected Output: [15, 3, 60, 0] #5 Input: world: [["#","#","#","#",".",".",".","#","."], ["#","#","#","#","#","#","#",".","#"], [".",".",".",".",".",".",".","#","#"], [".",".","#",".",".","#",".",".","."], [".",".",".",".","#","#",".","#","#"], ["#",".",".",".",".",".","#","#","#"], ["#",".",".",".","#","#","#","#","."], ["#",".",".",".",".","#",".",".","."], [".",".","#","#","#",".","#","#","."]] firstInfected: [3, 5] Output: [1, 2, 1, 34] Expected Output: [4, 2, 1, 34]
+ */
